0d9d79e75e81ade34b7a4c960ccd00eb804a7efa
Sebastian <sebastian@MacBook-Pro-tilhrende-Sebastian.local>
2021-05-31 17:10:39 +0200

    get by date

--- Yahoo finance Scripts/stock_price_loader.py
+++ Yahoo finance Scripts/stock_price_loader.py
@@ -8,6 +8,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from sqlalchemy.sql.expression import join
+from datetime import datetime, timedelta
 
 engine = create_engine('postgresql://postgres:password@localhost:5432/examDB', echo=False)
 
@@ -37,8 +38,10 @@ class Price_Records(Base):
     price = Column(Integer)
     stock_ticker = Column(String(10), ForeignKey('stocks.ticker'))
     date = Column(Date)
-    def __init__(self, stock_ticker):
+    def __init__(self, stock_ticker, price, date):
         self.stock_ticker = stock_ticker
+        self.date = date
+        self.price = price
 
 
 
@@ -49,15 +52,16 @@ ma = Marshmallow(app)
 
 class PriceSchema(ma.Schema):
     class Meta:
-        fields = (['ticker', 'price', 'date'])
+        fields = (['stock_ticker', 'price', 'date'])
 
 prices_schema = PriceSchema(many = True)
 
-@app.route('/prices/<string:ticker>')
-def index(ticker):
+@app.route('/prices/<string:ticker>/<string:date>')
+def index(ticker, date):
 
-    all_prices = session.query(Price_Records.price, Date.date).join(Price_Records, Date.id == Price_Records.date_id).filter(Price_Records.stock_ticker == ticker)
-    #all_prices  = session.query(func.public.get_stock("GME", 7)).all()
+    all_prices = session.query(Price_Records.price, Price_Records.date).filter(Price_Records.stock_ticker == 'GME', Price_Records.date == date).all()
+    #session.query(Price_Records.price, Date.date).join(Price_Records, Date.id == Price_Records.date_id).filter(Price_Records.stock_ticker == ticker)
+    #all_prices  = session.query(func.public.get_stock("GME")).all()
     print(all_prices)
     result = prices_schema.dump(all_prices)
     return jsonify(result)
